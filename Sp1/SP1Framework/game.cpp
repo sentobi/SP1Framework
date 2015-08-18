8cff96411378471048e648729c39e4cdb8c72927
sentobi <howcomee@outlook.com>
2015-08-18 14:17:39 +0800

    Added map rendering and collision (collision has problems with diagonal inputs, this needs to be worked on) So far, only 2 level maps have been designed

--- Sp1/SP1Framework/game.cpp
+++ Sp1/SP1Framework/game.cpp
@@ -5,12 +5,131 @@
 #include "Framework\console.h"
 #include <iostream>
 #include <iomanip>
+#include <string>
+
+using std::string;
 
 double elapsedTime;
 double deltaTime;
 bool keyPressed[K_COUNT];
 COORD charLocation;
 COORD consoleSize;
+
+void maprender();
+void maze1(int& rows, int& cols);
+void maze2(int& rows, int& cols);
+void mapgenerator(int rows, int cols);
+
+char ** maze = 0;
+
+void maprender() {
+
+	int rows = 0;
+	int cols = 0;
+
+	if (1) {
+		maze1(rows, cols);
+	}
+	else if (2) {
+		maze2(rows, cols);
+	}
+
+	mapgenerator(rows, cols);
+
+}
+
+void mapgenerator(int rows, int cols) {
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < cols; ++j) {
+			if (maze[i][j] == '@') { //teleporter
+				colour(0x0A);
+			}
+			else if (maze[i][j] == '$') { //item
+				colour(0x0B);
+			}
+			else if (maze[i][j] == '*') { //exit
+				colour(0x0D);
+			}
+			else {
+				colour(0x0F);
+			}
+			std::cout << maze[i][j];
+		}
+		std::cout << std::endl;
+	}
+
+}
+
+void maze1(int& rows, int& cols) {
+
+	string map1[21] = {
+		"##################",
+		"# #@             #",
+		"# ### ######## # #",
+		"#     #  #   # # #",
+		"### ### ## # # # #",
+		"#   #    # # # # #",
+		"# ###### # #@# # #",
+		"#     #@   ### ###",
+		"##### ######     #",
+		"#$# # # *  # #####",
+		"# # ###    #     #",
+		"#   #@#    ##### #",
+		"# ### #    #     #",
+		"#     #   @# # # #",
+		"# ### ######## # #",
+		"# # # #@#   ## # #",
+		"### # # # # ## # #",
+		"#@# #   # #    # #",
+		"# # ##### # ## # #",
+		"#         # #  #@#",
+		"##################" };
+
+	rows = 21;
+	maze = new char * [rows];
+	cols = 18;
+
+	for (int i = 0; i < rows; ++i) {
+		maze[i] = new char[cols];
+		string temp = map1[i];
+		for (int j = 0; j < cols; ++j) {
+			maze[i][j] = temp[j];
+		}
+	}
+
+}
+
+void maze2(int& rows, int& cols) {
+
+	string map2[13] = {
+		"########################",
+		"#      #       #       #",
+		"#### # #  #### # ##### #",
+		"#    #    #  # #     # #",
+		"# #########  # #     # #",
+		"#      #    ##       # #",
+		"#### #############   # #",
+		"#    #     *     #   # #",
+		"########################",
+		"#      #    #          #",
+		"#  #####  ###########  #",
+		"#      #               #",
+		"########################" };
+
+	rows = 13;
+	maze = new char * [rows];
+	cols = 24;
+
+	for (int i = 0; i < rows; ++i) {
+		maze[i] = new char[cols];
+		string temp = map2[i];
+		for (int j = 0; j < cols; ++j) {
+			maze[i][j] = temp[j];
+		}
+	}
+
+}
 
 void init()
 {
@@ -31,8 +150,10 @@ void init()
     consoleSize.Y = csbi.srWindow.Bottom + 1;
 
     // set the character to be in the center of the screen.
-    charLocation.X = consoleSize.X / 2;
-    charLocation.Y = consoleSize.Y / 2;
+    charLocation.X = 1;
+    charLocation.Y = 1;
+
+	//set values as the character's coordinates
 
     elapsedTime = 0.0;
 }
@@ -58,26 +179,47 @@ void update(double dt)
     elapsedTime += dt;
     deltaTime = dt;
 
+	int charX = charLocation.Y;
+	int charY = charLocation.X;
+
     // Updating the location of the character based on the key press
+
     if (keyPressed[K_UP] && charLocation.Y > 0)
     {
-        Beep(1440, 30);
-        charLocation.Y--; 
+		if (maze[charX-1][charY] != '#') {
+			Beep(1440, 30);
+			charLocation.Y--;
+		}
     }
     if (keyPressed[K_LEFT] && charLocation.X > 0)
-    {
-        Beep(1440, 30);
-        charLocation.X--; 
+	{
+		if (maze[charX][charY-1] == '#') {
+			std::cout << "Collision detected" << std::endl;
+		}
+		else {
+			Beep(1440, 30);
+			charLocation.X--; 
+		}
     }
     if (keyPressed[K_DOWN] && charLocation.Y < consoleSize.Y - 1)
     {
-        Beep(1440, 30);
-        charLocation.Y++; 
+		if (maze[charX+1][charY] == '#') {
+			std::cout << "Collision detected" << std::endl;
+		}
+		else {
+			Beep(1440, 30);
+			charLocation.Y++; 
+		}
     }
     if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1)
     {
-        Beep(1440, 30);
-        charLocation.X++; 
+		if (maze[charX][charY+1] == '#') {
+			std::cout << "Collision detected" << std::endl;
+		}
+		else {
+			Beep(1440, 30);
+			charLocation.X++; 
+		}
     }
 
     // quits the game if player hits the escape key
@@ -98,23 +240,18 @@ void render()
 	                        0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
 	                        0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
 	                        };
-	
-	for (int i = 0; i < 12; ++i)
-	{
-		gotoXY(3*i,i+1);
-		colour(colors[i]);
-		std::cout << "WOW";
-	}
+
+	maprender();
 
     // render time taken to calculate this frame
-    gotoXY(70, 0);
+    /*gotoXY(70, 0);
     colour(0x1A);
     std::cout << 1.0 / deltaTime << "fps" << std::endl;
   
     gotoXY(0, 0);
     colour(0x59);
     std::cout << elapsedTime << "secs" << std::endl;
-
+	*/
     // render character
     gotoXY(charLocation);
     colour(0x0C);
